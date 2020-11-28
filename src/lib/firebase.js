e64078c833aaa45a525aef6c1dbc7ee544f5e751
Param Shah <paramshah09@gmail.com>
2020-11-28 22:42:55 +0530

    firebase keys for VA project

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,12 @@
 const Config = {
-    apiKey: "AIzaSyAT35a6wJBeM4T92b0vHyLefh5QzTC92NA",
-    authDomain: "mentalhealth-2e530.firebaseapp.com",
-    databaseURL: "https://mentalhealth-2e530.firebaseio.com",
-    projectId: "mentalhealth-2e530",
-    storageBucket: "mentalhealth-2e530.appspot.com",
-    messagingSenderId: "928659331136",
-    appId: "1:928659331136:web:2b058e4fc5607638c3e7a5",
-    measurementId: "G-EHD7XP7RQZ"
+    apiKey: "AIzaSyBDlLqLVi5RohP9icH762-KnXovwVfrtDE",
+    authDomain: "veterans-affairs-78bd2.firebaseapp.com",
+    databaseURL: "https://veterans-affairs-78bd2.firebaseio.com",
+    projectId: "veterans-affairs-78bd2",
+    storageBucket: "veterans-affairs-78bd2.appspot.com",
+    messagingSenderId: "206326879131",
+    appId: "1:206326879131:web:828dd33f6144f3d7f8473e",
+    measurementId: "G-8LHPYJVH5J"
 };
 
 export default Config;

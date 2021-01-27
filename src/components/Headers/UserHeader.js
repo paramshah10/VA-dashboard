218757c3402bd95a66d1c1ef7ec6cf2be3d9cb26
Tyler Davis <tylerdavis@berkeley.edu>
2021-01-26 19:41:22 -0800

    more john doe changes

--- src/components/Headers/UserHeader.js
+++ src/components/Headers/UserHeader.js
@@ -40,7 +40,7 @@ class UserHeader extends React.Component {
           <Container className="d-flex align-items-center" fluid>
             <Row>
               <Col lg="7" md="10">
-                <h1 className="display-2 text-white">Hello Param</h1>
+                <h1 className="display-2 text-white">Hello John Doe</h1>
                 <p className="text-white mt-0 mb-5">
                   This is your profile page. You can see the progress you've
                   made with your work and manage your projects or assigned tasks
